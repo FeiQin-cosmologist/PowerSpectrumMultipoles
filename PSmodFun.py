8620f28f44bf48445be43560dba039233a5113b5
Fei QIN <75773209+FeiQin-cosmologist@users.noreply.github.com>
2024-07-30 11:29:10 +0900

    Add files via upload

--- PSmodFun.py
+++ PSmodFun.py
@@ -6,7 +6,16 @@ import emcee
 import camb
 from camb import model 
 from PSwincon import *
+
+
+
  
+
+
+
+
+ 
+
 
 
 
@@ -49,7 +58,7 @@ def Fmn(m,n,r,x,y):
     return FMN
 
 def Imn_inner_integ(x,m,n,k,r,Pl_spline):
-    y    = np.sqrt(1.0+r*r-2.0*r*x) 
+    y    = np.sqrt(1.0+r*r-2.0*r*x)#x=cos theta
     Plkq = sp.interpolate.splev(np.log10(k*y), Pl_spline, der=0)
     return Fmn(m,n,r,x,y)*10.0**(Plkq)
 
@@ -206,11 +215,11 @@ def K_mn(kmod,Pl_spline,ks):
         KSmn[i,0] = Ks00*ks[i]*ks[i]*ks[i]/(4.0*math.pi*math.pi) 
         
         K01,errin = sp.integrate.quad(Kmn_outer_integ,kmod[0],kmod[-1],args=(0,1,0,ks[i],Pl_spline),epsabs=0.0,epsrel=1.0e-3) 
-        KMN[i,0,1]= K01*ks[i]*ks[i]*ks[i]/(4.0*math.pi*math.pi)#-norm
+        KMN[i,0,1]= K01*ks[i]*ks[i]*ks[i]/(4.0*math.pi*math.pi) 
         Ks01,errin= sp.integrate.quad(Kmn_outer_integ,kmod[0],kmod[-1],args=(0,1,1,ks[i],Pl_spline),epsabs=0.0,epsrel=1.0e-3) 
-        KSmn[i,1] = Ks01*ks[i]*ks[i]*ks[i]/(4.0*math.pi*math.pi)#-4.0/9.0*norm
+        KSmn[i,1] = Ks01*ks[i]*ks[i]*ks[i]/(4.0*math.pi*math.pi) 
         Ks02,errin= sp.integrate.quad(Kmn_outer_integ,kmod[0],kmod[-1],args=(0,2,1,ks[i],Pl_spline),epsabs=0.0,epsrel=1.0e-3) 
-        KSmn[i,2] = Ks02*ks[i]*ks[i]*ks[i]/(4.0*math.pi*math.pi)#-2.0/3.0*norm
+        KSmn[i,2] = Ks02*ks[i]*ks[i]*ks[i]/(4.0*math.pi*math.pi) 
 
         K10,errin = sp.integrate.quad(Kmn_outer_integ,kmod[0],kmod[-1],args=(1,0,0,ks[i],Pl_spline),epsabs=0.0,epsrel=1.0e-3) 
         KMN[i,1,0]= K10*ks[i]*ks[i]*ks[i]/(4.0*math.pi*math.pi)
@@ -318,54 +327,60 @@ def PSlin_Integrations( KmodLim,hub,ombh2,omch2,ns, As,out_dir ):
 # 3. calculate model mom-PS:---------------------------------------------------
 def PijFun(parm,PT,MU,pstyp):
     if(pstyp=='auto'):
-        f, b1,b2,bs,b3nl, sigma_v1,sigma_v2,sigma_v3,sigma8rat = parm
+        f, b1,b2,bs,b3nl, sigv1_squre,sigv2_squre,sigvf_squre,sigma8rat = parm
     if(pstyp=='crs'):  
-        f, b1,b2,bs,b3nl, b1v,b2v,bsv,b3nlv, sigma_v1,sigma_v2,sigma_v3, sigmav_v1,sigmav_v2,sigmav_v3,sigma8rat = parm  
+        f, b1,b2,bs,b3nl, b1v,b2v,bsv,b3nlv, sigv1_squre,sigv2_squre,sigvf_squre,  sigvv1_squre,sigvv2_squre,sigvvf_squre,  sigma8rat = parm  
     k  =PT[0,0:] ; Pl =PT[1,0:]   
     I00=PT[2,0:]  ;I01=PT[3,0:]  ;I02=PT[4,0:]  ;I03=PT[5,0:]  ;I10=PT[6,0:]  ;I11=PT[7,0:]  ;I12=PT[8,0:]  ;I13=PT[9,0:] ;
     I20=PT[10,0:] ;I21=PT[11,0:] ;I22=PT[12,0:] ;I23=PT[13,0:] ;I30=PT[14,0:] ;I31=PT[15,0:] ;I32=PT[16,0:] ;I33=PT[17,0:] ;
     J00=PT[18,0:] ;J01=PT[19,0:];J02=PT[20,0:]; J10=PT[21,0:]; J11=PT[22,0:];  J20=PT[23,0:]; K00=PT[24,0:]; Ks00=PT[25,0:];
     K01=PT[26,0:]; Ks01=PT[27,0:];Ks02=PT[28,0:];K10=PT[29,0:];Ks10=PT[30,0:]; K11=PT[31,0:];
-    Ks11=PT[32,0:];K20=PT[33,0:];Ks20=PT[34,0:];K30=PT[35,0:];Ks30=PT[36,0:];  sig3_squre=PT[37,0:];
+    Ks11=PT[32,0:];K20=PT[33,0:];Ks20=PT[34,0:];K30=PT[35,0:];Ks30=PT[36,0:];  sig3_squre=PT[37,0:];#Intg_norm=PT[38,0:];   
     sig4_squre=1.0/(24.0*math.pi*math.pi)*(sigma8rat**2*sp.integrate.simps( (I23+2./3.*I32+I33/5.) /k**2,k)) 
-    sigvL_squre=0     
+    
+    #sigvL_squre=1.0/(6.0*math.pi**2)*sp.integrate.simps(sigma8rat*k*k*(Pl + sigma8rat*(2.0*I11 + 6.*k*k*Pl*J11)), k) 
+    sigvL_squre=sigv1_squre +0-0     ;    sigv1_squre =0; sigv2_squre=0;    sigvv1_squre=0; sigvv2_squre=0;  
+    
     P_00=np.zeros((len(k),len(MU)));P_01=np.zeros((len(k),len(MU)));P_02=np.zeros((len(k),len(MU)))
     P_03=np.zeros((len(k),len(MU)));P_04=np.zeros((len(k),len(MU)));P_11=np.zeros((len(k),len(MU)))
     P_12=np.zeros((len(k),len(MU)));P_13=np.zeros((len(k),len(MU)));P_22=np.zeros((len(k),len(MU)))
     for ss in range(len(MU)):
         mu=MU[ss]
-        if(pstyp=='auto'):
-            P00=b1*b1 *sigma8rat *(Pl+2.*sigma8rat*(I00+3.*k*k*Pl*J00))+ (b1+b1)*sigma8rat**2*(b2*K00+bs*Ks00+b3nl*sig3_squre*Pl)+sigma8rat**2*(1./2.*b2*b2*K01+1./2.*bs*bs*Ks01+b2*bs*Ks02)
-            P01=f*b1  *sigma8rat *(Pl+2.*sigma8rat*(I01+b1*I10+3.*k*k*Pl*(J01+b1*J10))-b2*sigma8rat*K11-bs*sigma8rat*Ks11)-f*sigma8rat**2*(b2*K10+bs*Ks10+b3nl*sig3_squre*Pl)
-            P02=f*f*b1*sigma8rat**2*(I02+mu**2*I20+2.*k*k*Pl*(J02+mu**2*J20))-f*f*k*k*(sigvL_squre+sigma_v1 )* P00-f*f*sigma8rat**2*(b2*(K20+mu**2*K30)+bs*(Ks20+mu**2*Ks30))
-            P03=-f*f*k*k*(sigvL_squre+sigma_v2)*P01
-            P04=-1./2.*f*f*f*f*b1*k*k*(sigvL_squre+sigma_v3)*sigma8rat**2*(I02+mu**2*I20+2.*k*k*Pl*(J02+mu**2*J20))+1./4.*f*f*f*f*b1*b1*k**4*P00*((sigvL_squre+sigma_v3)**2+sig4_squre )
-            P11=f*f*sigma8rat*(mu**2*(Pl+sigma8rat*(2.*I11+(b1+b1+b1+b1)*I22+b1*b1*I13+6.*k**2*Pl*(J11+(b1+b1)*J10)))+b1*b1*sigma8rat*I31)
-            P12=f*f*f*sigma8rat**2*(I12+mu**2*I21-b1*(I03+mu**2*I30)+2.*k**2*Pl*(J02+mu**2*J20))-f*f*k*k*(sigvL_squre+sigma_v1 )* P01
-            P13=-f*f*k*k* f*f*sigma8rat*(    (sigvL_squre+sigma_v2)*mu**2*(Pl+sigma8rat*(2.*I11+(b1+b1+b1+b1)*I22+6.*k*k*Pl*(J11+(b1+b1)*J10)))+ (sigvL_squre+sigma_v1 )*b1*b1*sigma8rat*(mu**2*I13+I31) )
-            P22=1./4.*f*f*f*f*sigma8rat**2*(I23+2.*mu**2*I32+mu**4*I33)+f*f*f*f*k**4*(sigvL_squre+sigma_v1 )**2 *P00-f*f*k*k*(sigvL_squre+sigma_v1 )*(2.*P02-f*f*sigma8rat**2*(b2*(K20+mu**2*K30)+bs*(Ks20+mu**2*Ks30)))            
+        if(pstyp=='auto'):# Note: sig3_squre is not sigv3_squre, and sig4_squre is not sigv4_squre, they are different parameters, do not confused with them.
+            P00=(b1+b1)*sigma8rat**2*(b2*K00+bs*Ks00)+b1*b1*sigma8rat*(Pl+2.*sigma8rat*(I00+3.*k*k*Pl*J00))+1./2.*sigma8rat**2*(b2*b2*K01+bs*bs*Ks01+2.*b2*bs*Ks02+4.*b3nl*sig3_squre*Pl)
+            P01=f*b1*sigma8rat*(Pl+2.*sigma8rat*(I01+b1*I10+3.*k*k*Pl*(J01+b1*J10))-b2*sigma8rat*K11-bs*sigma8rat*Ks11)-f*sigma8rat**2*(b2*K10+bs*Ks10+b3nl*sig3_squre*Pl)
+            P02=f*f*b1*sigma8rat**2*(I02+mu**2*I20+2.*k*k*Pl*(J02+mu**2*J20))-f*f*k*k*(sigvL_squre+sigv1_squre/f**4 )* P00+f*f*sigma8rat**2*(b2*(K20+mu**2*K30)+bs*(Ks20+mu**2*Ks30))
+            P03=-f*f*k*k*(sigvL_squre+sigv2_squre/f**4)*P01
+            P04=-1./2.*f*f*f*f*b1*k*k*(sigvL_squre+sigv1_squre/f**4)*sigma8rat**2*(I02+mu**2*I20+2.*k*k*Pl*(J02+mu**2*J20)) +1./4.*f*f*f*f*b1*b1*k**4*P00*((sigvL_squre+sigv1_squre/f**4)**2+sig4_squre/3. )
+            P11=f*f*sigma8rat*(mu**2*(Pl+sigma8rat*(2.*I11+(b1+b1+b1+b1)*I22+b1*b1*I13+6.*k**2*Pl*(J11+(b1+b1)*J10)))  +b1*b1*sigma8rat*I31)
+            P12=f*f*f*sigma8rat**2*(I12+mu**2*I21-b1*(I03+mu**2*I30)+2.*k**2*Pl*(J02+mu**2*J20))   -f*f*k*k*(sigvL_squre+sigv1_squre/f**4)* P01+2.*f*f*f*k*k*sigma8rat**2*(I01+I10+3.*k*k*Pl*(J01+J10))   *sigvf_squre 
+            P13=-f*f*k*k*f*f*sigma8rat*((sigvL_squre+sigv2_squre/f**4)*mu**2*(Pl+sigma8rat*(2.*I11+(b1+b1+b1+b1)*I22 +6.*k*k*Pl*(J11+(b1+b1)*J10))) +(sigvL_squre+sigv1_squre/f**4)*b1*b1*sigma8rat*(mu**2*I13+I31) )
+            P22=1./4.*f*f*f*f*sigma8rat**2*(I23+2.*mu**2*I32+mu**4*I33)+f*f*f*f*k**4*(sigvL_squre+sigv1_squre/f**4 )**2 *P00-f*f*k*k*(sigvL_squre+sigv1_squre/f**4 )*(2.*P02-f*f*sigma8rat**2*(b2*(K20+mu**2*K30)+bs*(Ks20+mu**2*Ks30)))          
         if(pstyp=='crs'): 
-            P00=b1*b1 *sigma8rat *(Pl+2.*sigma8rat*(I00+3.*k*k*Pl*J00))+ (b1+b1)*sigma8rat**2*(b2*K00+bs*Ks00+b3nl*sig3_squre*Pl)+sigma8rat**2*(1./2.*b2*b2*K01+1./2.*bs*bs*Ks01+b2*bs*Ks02)
-            P01=f*b1  *sigma8rat *(Pl+2.*sigma8rat*(I01+b1*I10+3.*k*k*Pl*(J01+b1*J10))-b2*sigma8rat*K11-bs*sigma8rat*Ks11)-f*sigma8rat**2*(b2*K10+bs*Ks10+b3nl*sig3_squre*Pl)
-            P02=f*f*b1*sigma8rat**2*(I02+mu**2*I20+2.*k*k*Pl*(J02+mu**2*J20))-f*f*k*k*(sigvL_squre+sigma_v1 )* P00-f*f*sigma8rat**2*(b2*(K20+mu**2*K30)+bs*(Ks20+mu**2*Ks30))
-            P03=-f*f*k*k*(sigvL_squre+sigma_v2)*P01
-            P04=-1./2.*f*f*f*f*b1*k*k*(sigvL_squre+sigma_v3)*sigma8rat**2*(I02+mu**2*I20+2.*k*k*Pl*(J02+mu**2*J20))+1./4.*f*f*f*f*b1*b1*k**4*P00*((sigvL_squre+sigma_v3)**2+sig4_squre )
-            P11=f*f*sigma8rat*(mu**2*(Pl+sigma8rat*(2.*I11+(b1+b1+b1+b1)*I22+b1*b1*I13+6.*k**2*Pl*(J11+(b1+b1)*J10)))+b1*b1*sigma8rat*I31)
-            P12=f*f*f*sigma8rat**2*(I12+mu**2*I21-b1*(I03+mu**2*I30)+2.*k**2*Pl*(J02+mu**2*J20))-f*f*k*k*(sigvL_squre+sigma_v1 )* P01
-            P13=-f*f*k*k* f*f*sigma8rat*(    (sigvL_squre+sigma_v2)*mu**2*(Pl+sigma8rat*(2.*I11+(b1+b1+b1+b1)*I22+6.*k*k*Pl*(J11+(b1+b1)*J10)))+ (sigvL_squre+sigma_v1 )*b1*b1*sigma8rat*(mu**2*I13+I31) )
-            P22=1./4.*f*f*f*f*sigma8rat**2*(I23+2.*mu**2*I32+mu**4*I33)+f*f*f*f*k**4*(sigvL_squre+sigma_v1 )**2 *P00-f*f*k*k*(sigvL_squre+sigma_v1 )*(2.*P02-f*f*sigma8rat**2*(b2*(K20+mu**2*K30)+bs*(Ks20+mu**2*Ks30)))            
+            P00=(b1+b1)*sigma8rat**2*(b2*K00+bs*Ks00)+b1*b1*sigma8rat*(Pl+2.*sigma8rat*(I00+3.*k*k*Pl*J00))+1./2.*sigma8rat**2*(b2*b2*K01+bs*bs*Ks01+2.*b2*bs*Ks02+4.*b3nl*sig3_squre*Pl)
+            P01=f*b1*sigma8rat*(Pl+2.*sigma8rat*(I01+b1*I10+3.*k*k*Pl*(J01+b1*J10))-b2*sigma8rat*K11-bs*sigma8rat*Ks11)-f*sigma8rat**2*(b2*K10+bs*Ks10+b3nl*sig3_squre*Pl)
+            P02=f*f*b1*sigma8rat**2*(I02+mu**2*I20+2.*k*k*Pl*(J02+mu**2*J20))-f*f*k*k*(sigvL_squre+sigv1_squre/f**4 )* P00+f*f*sigma8rat**2*(b2*(K20+mu**2*K30)+bs*(Ks20+mu**2*Ks30))
+            P03=-f*f*k*k*(sigvL_squre+sigv2_squre/f**4)*P01
+            P04=-1./2.*f*f*f*f*b1*k*k*(sigvL_squre+sigv1_squre/f**4)*sigma8rat**2*(I02+mu**2*I20+2.*k*k*Pl*(J02+mu**2*J20)) +1./4.*f*f*f*f*b1*b1*k**4*P00*((sigvL_squre+sigv1_squre/f**4)**2+sig4_squre/3. )
+            P11=f*f*sigma8rat*(mu**2*(Pl+sigma8rat*(2.*I11+(b1+b1+b1+b1)*I22+b1*b1*I13+6.*k**2*Pl*(J11+(b1+b1)*J10)))  +b1*b1*sigma8rat*I31)
+            P12=f*f*f*sigma8rat**2*(I12+mu**2*I21-b1*(I03+mu**2*I30)+2.*k**2*Pl*(J02+mu**2*J20))   -f*f*k*k*(sigvL_squre+sigv1_squre/f**4)* P01+2.*f*f*f*k*k*sigma8rat**2*(I01+I10+3.*k*k*Pl*(J01+J10))   *sigvf_squre 
+            P13=-f*f*k*k*f*f*sigma8rat*((sigvL_squre+sigv2_squre/f**4)*mu**2*(Pl+sigma8rat*(2.*I11+(b1+b1+b1+b1)*I22 +6.*k*k*Pl*(J11+(b1+b1)*J10))) +(sigvL_squre+sigv1_squre/f**4)*b1*b1*sigma8rat*(mu**2*I13+I31) )
+            P22=1./4.*f*f*f*f*sigma8rat**2*(I23+2.*mu**2*I32+mu**4*I33)+f*f*f*f*k**4*(sigvL_squre+sigv1_squre/f**4 )**2 *P00-f*f*k*k*(sigvL_squre+sigv1_squre/f**4 )*(2.*P02-f*f*sigma8rat**2*(b2*(K20+mu**2*K30)+bs*(Ks20+mu**2*Ks30)))          
         P_00[:,ss]=P00;   P_01[:,ss]=P01;   P_02[:,ss]=P02
         P_03[:,ss]=P03;   P_04[:,ss]=P04;   P_11[:,ss]=P11
         P_12[:,ss]=P12;   P_13[:,ss]=P13;  P_22[:,ss]=P22 
     return   P_00,P_01,P_02,P_03,P_04,P_11,P_12,P_13,P_22  
 def Pk_MOD(params,sigma8_fid,PT,ps_type, Growthz ):
     
-    fsigma8,b1sigma8,b2sigma8,    sigma_v  = params 
+    fsigma8,  b1sigma8,b2sigma8,b3nlsigma8,   sigmav_squre  = params 
     
+    sigmav_squre=np.abs(sigmav_squre)
+    sigmavf_squre=np.abs(sigmav_squre)
     b1vsigma8,b2vsigma8 = b1sigma8, b2sigma8
-    sigma_v1 ,sigma_v2  = sigma_v , sigma_v  
-    sigmav_v1,sigmav_v2 = sigma_v , sigma_v  
-    sigma_v3, sigmav_v3 = sigma_v1, sigmav_v1  
+    b3nlvsigma8 =  b3nlsigma8
+    sigma_v1_squre ,sigma_v2_squre  = sigmav_squre , sigmav_squre
+    sigmav_v1_squre,sigmav_v2_squre = sigmav_squre , sigmav_squre  
+    sigmav_vf_squre=sigmavf_squre
     
     # normalize parms: 
     f = fsigma8/sigma8_fid
@@ -374,32 +389,32 @@ def Pk_MOD(params,sigma8_fid,PT,ps_type, Growthz ):
     b1v       = b1vsigma8/sigma8_fid
     b2v       = b2vsigma8/sigma8_fid 
     bsv       = -4.0/7.0*(b1v-1.0)
-    b3nlv     = 32.0/315.0*(b1v-1.0)    
+    b3nlv     = b3nlvsigma8/sigma8_fid # 32.0/315.0*(b1v-1.0)    
     # density field:  
     b1        = b1sigma8/sigma8_fid 
     b2        = b2sigma8/sigma8_fid
     bs        = -4.0/7.0*(b1-1.0)
-    b3nl      = 32.0/315.0*(b1-1.0)
+    b3nl      = b3nlsigma8/sigma8_fid #32.0/315.0*(b1-1.0)
     # model PS:
     P_model=[];PSTYP=[]
     mu = np.linspace(0.0, 1.0, 100)    
     if ('den' in ps_type):
-        P_00,P_01,P_02,P_03,P_04,P_11,P_12,P_13,P_22 =PijFun( [f, b1,b2,bs,b3nl, sigma_v1,sigma_v2,sigma_v3,sigma8rat ],PT,mu, 'auto')
+        P_00,P_01,P_02,P_03,P_04,P_11,P_12,P_13,P_22 =PijFun( [f, b1,b2,bs,b3nl, sigma_v1_squre,sigma_v2_squre, sigmavf_squre,sigma8rat ],PT,mu, 'auto')
         P_den  = P_00 + mu**2*(2.0*P_01 + P_02 + P_11) + mu**4*(P_03 + P_04 + P_12 + P_13 + 1.0/4.0*P_22)
         P_0den = (2.0*0 + 1.0)  * sp.integrate.simps(P_den,mu,axis=1)
-        P_2den = (2.0*2 + 1.0)  * sp.integrate.simps( ((3.*(mu**2)-1.)/2.)*P_den,mu,axis=1) 
+        P_2den = (2.0*2 + 1.0)  * sp.integrate.simps( ((3.*(mu**2)-1.)/2.)*P_den,mu,axis=1)
         P_4den = (2.0*4 + 1.0)  * sp.integrate.simps( ((35.*(mu**4)-30.*(mu**2)+3.)/8. )*P_den,mu,axis=1)  
         P_model.append([P_0den,P_2den,P_4den]) ; PSTYP.append( 'den' )
     if ('mom' in ps_type):
-        P_00,P_01,P_02,P_03,P_04,P_11,P_12,P_13,P_22 =PijFun( [f, b1v,b2v,bsv,b3nlv, sigmav_v1,sigmav_v2,sigmav_v3,sigma8rat ],PT,mu, 'auto')
+        P_00,P_01,P_02,P_03,P_04,P_11,P_12,P_13,P_22 =PijFun( [f, b1v,b2v,bsv,b3nlv, sigmav_v1_squre,sigmav_v2_squre, sigmav_vf_squre,sigma8rat ],PT,mu, 'auto')
         P_mom  = ((1.0e4)/(1.0**2)/(PT[0,0:]*PT[0,0:])*(P_11 + mu**2*(2.0*P_12 + 3.0*P_13 + P_22)).T).T
         P_0mom = (2.0*0 + 1.0)  * sp.integrate.simps(P_mom,mu,axis=1)
         P_2mom = (2.0*2 + 1.0)  * sp.integrate.simps( ((3.*(mu**2)-1.)/2.)*P_mom,mu,axis=1) 
         P_4mom = (2.0*4 + 1.0)  * sp.integrate.simps( ((35.*(mu**4)-30.*(mu**2)+3.)/8. )*P_mom,mu,axis=1)  
         P_model.append([P_0mom,P_2mom,P_4mom])  ;  PSTYP.append( 'mom' )
     if ('crs' in ps_type):
-        P_00,P_01,P_02,P_03,P_04,P_11,P_12,P_13,P_22 =PijFun( [f, b1,b2,bs,b3nl, b1v,b2v,bsv,b3nlv, sigma_v1,sigma_v2, sigma_v3,sigmav_v1,sigmav_v2,sigmav_v3,sigma8rat],PT,mu, 'crs')
-        P_crs  = (1.0e2/PT[0,0:]*((mu*(P_01 + P_02 + P_11 + mu**2*(3.0/2.0*P_03 + 2.0*P_04 + 2.0*P_12 + 3.0*P_13 + 1.0/5.0*P_22))).T)).T
+        P_00,P_01,P_02,P_03,P_04,P_11,P_12,P_13,P_22 =PijFun( [f, b1,b2,bs,b3nl, b1v,b2v,bsv,b3nlv, sigma_v1_squre,sigma_v2_squre,  sigmavf_squre,sigmav_v1_squre,sigmav_v2_squre, sigmav_vf_squre,sigma8rat],PT,mu, 'crs')
+        P_crs  = (1.0e2/PT[0,0:]*((mu*(P_01 + P_02 + P_11 + mu**2*(3.0/2.0*P_03 + 2.0*P_04 + 2.0*P_12 + 3.0*P_13  + 1.0/2.0*P_22 ))).T)).T
         P_1crs = (2.0*1 + 1.0)  * sp.integrate.simps(mu*P_crs,mu,axis=1)
         P_3crs = (2.0*3 + 1.0)  * sp.integrate.simps( (1./2.*(5.*mu**3-3.*mu) )*P_crs,mu,axis=1)   
         P_model.append([P_1crs,P_3crs])  ; PSTYP.append( 'crs' )
@@ -559,41 +574,29 @@ def CHI2(params,    ind_fit,Cov_inv,OnlyL0,Sig8_fid, k_obs, Pk_obs,kmod_conv, WF
         VC=np.dot((Pk_obs -Pk_modc ),Cov_inv)
         chi_squared=np.dot(VC,(Pk_obs -Pk_modc ))           
     return chi_squared
-def tPDF(params,    Cp,Nmock,ind_fit,Cov_inv,OnlyL0,Sig8_fid, k_obs, Pk_obs,kmod_conv, WF_Kobs_Kmod,WF_rand,PT,ps_type, Growthz, WFCon_type ):
-    chi_squared=CHI2(params,    ind_fit,Cov_inv,OnlyL0,Sig8_fid, k_obs, Pk_obs,kmod_conv, WF_Kobs_Kmod,WF_rand,PT,ps_type, Growthz, WFCon_type ) 
-    Pb   = np.log(  Cp*(1.0+chi_squared/(Nmock-1.0))) * (-Nmock/2.0)         
-    return -Pb
 # 5. the posterior and prior of MCMC:--------------------------------------
 def lnprior(params):
-    fsigma8,    b1sigma8,b2sigma8,      sigma_v   = params   
+    fsigma8,  b1sigma8,b2sigma8,b3nlsigma8,   sigmav_squre    = params   
     if(0.  <fsigma8<1.5): pdf=1.
     else:return -np.inf
     if(0.  <b1sigma8<3.):pdf=1.
     else:return -np.inf
-    if(0.  <b2sigma8<3.):pdf=1.
+    if(-3.  <b2sigma8<3.):pdf=1.
     else:return -np.inf
-    if( 0.<sigma_v<250.):pdf=1.
+    if(-3.  <b3nlsigma8<3.):pdf=1.
+    else:return -np.inf
+    if( 0.<sigmav_squre<250.):pdf=1.
     else:return -np.inf
     return 0.0
 # 5.1: the post of MCMC:
-def lnpost_tPDF(params    ,   Cp,Nmock,ind_fit,Cov_inv,OnlyL0,Sig8_fid, k_obs, Pk_obs,kmod_conv, WF_Kobs_Kmod,WF_rand,PT,ps_type, Growthz, WFCon_type  ):
-    prior = lnprior(params )
-    if not np.isfinite(prior):
-        return -np.inf
-    like = tPDF(params ,Cp,Nmock,ind_fit,Cov_inv,OnlyL0,Sig8_fid, k_obs, Pk_obs,kmod_conv, WF_Kobs_Kmod,WF_rand,PT,ps_type, Growthz, WFCon_type )
-    return prior - like
 def lnpost_chi2(params    ,   Cp,Nmock,ind_fit,Cov_inv,OnlyL0,Sig8_fid, k_obs, Pk_obs,kmod_conv, WF_Kobs_Kmod,WF_rand,PT,ps_type, Growthz, WFCon_type  ):
     prior = lnprior(params )
     if not np.isfinite(prior):
         return -np.inf
     like = -0.5* CHI2(params ,ind_fit,Cov_inv,OnlyL0,Sig8_fid, k_obs, Pk_obs,kmod_conv, WF_Kobs_Kmod,WF_rand,PT,ps_type, Growthz, WFCon_type )
     return prior + like
-
-
  
-        
-        
-        
+ 
         
         
         
